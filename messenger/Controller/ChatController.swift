4d5dd7f367327d975a59cfa4c7b92b16cb55683d
FirsAlex <firs_alex@list.ru>
2022-03-05 10:51:42 +0300

    сцена Chat Controller

--- messenger/Controller/ChatController.swift
+++ messenger/Controller/ChatController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ChatController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet var dataTextField: UITextField!
+    @IBOutlet weak var dataTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     deinit{
+        removeForKeyboardNotifications()
         print("ChatController - deinit")
     }
     
@@ -21,6 +23,8 @@ class ChatController: UIViewController {
         let chatsCellNib = UINib(nibName: "ChatsCell", bundle: nil)
         //регистрация кастомной ячейки в табличном представлении
         tableView.register(chatsCellNib, forCellReuseIdentifier: "ChatsCell")
+        
+        registerForKeyboardNotifications()
         print("ChatController - viewDidLoad")
     }
     
@@ -31,8 +35,6 @@ class ChatController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        // Show keyboard by default
-        //dataTextField.becomeFirstResponder()
         print("ChatController - viewDidAppear")
     }
     
@@ -41,8 +43,30 @@ class ChatController: UIViewController {
         self.navigationController?.popToRootViewController(animated: true)
    }
     
+    // отправка
     @IBAction func send() {
         print("WORK!!!")
+    }
+    
+    //MARK: обрабатываем нотификации от клавиатуры
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func removeForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func kbWillShow(_ notification: Notification) {
+        let userInfo = notification.userInfo
+        let kbFrameSize = (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        scrollView.contentOffset = CGPoint(x: 0, y: kbFrameSize.height) //
+    }
+    
+    @objc func kbWillHide() {
+        scrollView.contentOffset = CGPoint.zero
     }
 
 }
@@ -58,7 +82,7 @@ extension ChatController: UITableViewDataSource {
         //получение переиспользуемой кастомной ячейки по ее идентификатору
         let chatsCell = tableView.dequeueReusableCell(withIdentifier: "ChatsCell", for: indexPath) as! ChatsCell
         //заполняем ячейку данными
-        chatsCell.nameContact.text = "Юлька"
+        chatsCell.nameContact.text = "Юлька \(indexPath.row)"
         chatsCell.lastMessageTime.text = "01.02.2022"
         chatsCell.symbol.attributedText = NSAttributedString(string: "\u{2713}\u{2713}", attributes: [.kern: -6])
         chatsCell.lastMessage.text = "Вы: Трам пам пам очень большой прибольшой текстище текстовый такой здаровый здоровенный егегей!!!"
@@ -68,7 +92,12 @@ extension ChatController: UITableViewDataSource {
 
 // MARK: - делегирование ChatController
 extension ChatController: UITableViewDelegate {
-
+    //MARK: обработка выделения строки
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        dataTextField.resignFirstResponder()
+        // снимаем выделение со строки
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
 //MARK: - расширение TableView
