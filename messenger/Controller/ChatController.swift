916db5a0da00cbb14ae9fa220d855c38ee3b3e39
FirsAlex <firs_alex@list.ru>
2022-03-11 21:51:39 +0300

    loadMessages

--- messenger/Controller/ChatController.swift
+++ messenger/Controller/ChatController.swift
@@ -93,7 +93,9 @@ class ChatController: UIViewController {
         }
         groupWaitResponseHttp.notify(qos: .userInteractive, queue: .main) { [self] in
             guard contact.sql.httpStatus?.statusCode == 200 else {
-                navigationController?.popViewController(animated: true)
+                contact.sql.httpStatus = nil
+                contact.showAlertMessage("Ошибка связи с БД", self)
+                navigationController?.popToRootViewController(animated: true)
                 return
             }
             if checkMessage {
